4a0a53ff8fc583df205a9eac17f1850615c4718d
TheShoShaDev <UserName@DESKTOP-5QP6OJV>
2023-12-05 02:13:25 +0700

    Добавлены тесты

--- MyLibraryClass.cs
+++ MyLibraryClass.cs
@@ -2,23 +2,22 @@
 {
 	public class MyArea
 	{
-		float FindCircleArea(float R)
+		public float FindCircleArea(float R)
 		{
 			return (float)3.14 * R * R;
 		}
 
-		float FideTriangeArea(float Side1, float Side2, float Side3)
+		public float FideTriangeArea(float Side1, float Side2, float Side3)
 		{
 			float HalfPerimetr = (Side1 + Side2 + Side3) / 2;
 			float MyFormula = HalfPerimetr * (HalfPerimetr - Side1) * (HalfPerimetr - Side2) * (HalfPerimetr - Side3);
-			return 0;
+			return (float)Math.Sqrt(MyFormula);
 		}
 
-
-		bool IsTriangleStraight(float A, float B, float C)
+		public bool IsTriangleStraight(float A, float B, float C)
 		{
 			bool result;
-			result = (Math.Pow(A,2) + Math.Pow(B,2)) == Math.Pow(C,2);
+			result = (Math.Pow(A, 2) + Math.Pow(B, 2)) == Math.Pow(C, 2);
 			return result;
 
 			/*Выражение:
@@ -30,5 +29,4 @@
 
 
 	}
-
 }
